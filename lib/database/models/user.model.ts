44384b5c2858c9761ed3ff7e1d13243c027292e8
Mohd Khan <anas@anass-MacBook-Pro.local>
2024-02-21 20:13:25 +0530

    initial commit

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,21 @@
+// task for creating model by ownself
+// clerkID, String , username , photo , firstName , lastName , planID , creditBalance
+
+import { Document, Schema, model, models } from "mongoose";
+
+ 
+const userScehema = new Schema({
+    clerkId : {type : String , required : true, unique : true}, 
+    email : {type : String , required : true , unique : true}, 
+    username : {type : String , required : true , unique : true}, 
+    photo : {type : String , required : true}, 
+    firstName : {type : String}, 
+    lastName : {type : String }, 
+    planID : {type : Number , default : 1},  
+    creditBalance : {type : Number , default : 10 }, 
+})
+
+
+const User = models?.User || model('User', userScehema)
+
+export default User;
